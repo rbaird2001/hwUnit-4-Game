a97c601e9156c665b0cb326089d26d5e51769806
RBaird2001 <rbaird2001@yahoo.com>
2019-08-03 00:04:23 -0600

    Improved gameboard for less up down scrolling. Added results detail in modal

--- assets/javascript/gameplay.js
+++ assets/javascript/gameplay.js
@@ -12,22 +12,27 @@ var objGame = {
     strSelOpp: "",
     intOppCharPower: 0,
     intOppCharLife: 0,
-    attackSet : false,
-    selPlayLife : 0,
-    selPlayPower : 0,
-    oppPlayLife : 0,
-    oppPlayPower :0,
-    playCount : 0,
+    attackSet: false,
+    selPlayLife: 0,
+    selPlayPower: 0,
+    oppPlayLife: 0,
+    oppPlayPower: 0,
+    playCount: 0,
     winCount: 0,
+    gameIntro: "Use drag and drop to select  characters.",
+    gameInstructions: "Drag your character in the designated box. Then drag the opponent to its designated spot. Use attack button to play.",
+    beginPlayButtonText: "Got it. Ready to Play",
     turtles: "i like turtles",
     setupPlayChars: function () {
         self = (this);
-        $("#rowChar").find(".imgChar").each(function () { 
-           self.arrPlayableChars.push(this.id); 
+        $("#rowChar").find(".imgChar").each(function () {
+            self.arrPlayableChars.push(this.id);
         });
         this.prepCharsForReset(this.arrPlayableChars)
         this.setupCharDrag(this.eleImgChars);
         this.setupDrop(this.eleSelChar);
+        //the called function is mainly used for battle outcomes but also works for intro detail.
+        this.showBattleOutcome(this.gameIntro,this.gameInstructions,this.beginPlayButtonText);
     },
     prepCharsForReset: function (parArray) {
         for (i = 0; i < parArray.length; i++) {
@@ -45,13 +50,13 @@ var objGame = {
             revert: "invalid"
         });
     },
-    setupDrop: function(parDrop){
+    setupDrop: function (parDrop) {
         self = this
         $(parDrop).droppable({
             drop: function (event, ui) {
                 var drag = $(ui.draggable).attr("id");
                 var drop = $(event.target).attr("id");
-                self.setupChars(drag,drop);
+                self.setupChars(drag, drop);
             }
         });
     },
@@ -75,7 +80,7 @@ var objGame = {
             this.initPowerDisplay();
             this.setupAttack();
         }
-        //TODO: Ensure no other chars can be put into selected car by disabling #imgSelChar ability
+    //TODO: Ensure no other chars can be put into selected car by disabling #imgSelChar ability
     },
     setupOpps: function (parDrag) {
         var idxChar = this.arrPlayableChars.indexOf(parDrag);
@@ -102,11 +107,6 @@ var objGame = {
         }
     },
     powerOpps: function (array) {
-        /**
-         * Take randomized opponent array and combine
-         *    with character powers array and create new
-         *    object assigning each character to its power
-         */
         this.objOppPower = {};
         for (i = 0; i < array.length; i++) {
             //Skip index 0 of arrCharsPower. It goes to the selected character
@@ -115,42 +115,62 @@ var objGame = {
             this.objOppPower[array[i]] = this.arrCharsPower[j];
         }
     },
-    initPowerDisplay: function(){
+    initPowerDisplay: function () {
         this.selPlayLife = this.intSelCharLife;
         this.selPlayPower = this.intSelCharPower;
         this.oppPlayLife = this.intOppCharLife;
         this.oppPlayPower = this.intOppCharPower;
     },
     setupAttack: function () {
-        if (this.attackSet != true){
+        if (this.attackSet != true) {
             $("#btnAtt").click(this.attack.bind(this));
             this.attackSet = true;
         }
     },
     attack: function () {
-            this.playCount++;
-            this.oppPlayLife -= this.intSelCharPower;
-            this.selPlayLife -= this.intOppCharPower;
+        this.playCount++;
+        this.oppPlayLife -= this.intSelCharPower;
+        this.selPlayLife -= this.intOppCharPower;
+        let battleOutcome = "";
+        let battleOutcomeDetail = "";
+        let closeButtonText = "";
         if (this.oppPlayLife <= 0) {
-            this.winCount ++
-            alert("victorious");
+            this.winCount++
+            battleOutcome = "VICTORY!"
+            battleOutcomeDetail = "You have fought well and defeated your opponent."
+            closeButtonText = "Continue playing"
             this.intSelCharLife += 35;
             this.intSelCharPower += 25;
             this.selPlayLife = this.intSelCharLife;
             this.selPlayPower = this.intSelCharPower
             removeOpp = "#" + this.strSelOpp;
             let remOppClass = $(removeOpp).attr("class") + " d-none";
-            $(removeOpp).attr("class",remOppClass);
+            $(removeOpp).attr("class", remOppClass);
             $("#selPower").text(this.intSelCharPower);
             $("#selLife").text(this.intSelCharLife);
         }
         else if (this.selPlayLife <= 0) {
-            alert("defeat");
+            battleOutcome = "Defeat."
+            battleOutcomeDetail = "Your opponent was to strong for you at this time. Try another opponent first."
+            closeButtonText = "Choose different opponent"
             this.newOpp();
-            if(this.playCount > 0)
-            this.selPlayLife = this.intSelCharLife;
-            this.selPlayPower = this.intSelCharPower;
+            if (this.playCount > 0)
+                this.selPlayLife = this.intSelCharLife;
+                this.selPlayPower = this.intSelCharPower;
         }
+        else {
+            battleOutcome = "Excellent."
+            battleOutcomeDetail = "You have weakened your opponent, but they are still able to fight. Try again."
+            closeButtonText = "Resume battle"
+        }
+        this.showBattleOutcome(battleOutcome,battleOutcomeDetail,closeButtonText)
+    },
+    showBattleOutcome: function(Outcome,OutcomeDetail,buttonText){
+        $("#battleOutcomeTitle").html(Outcome);
+        $("#battleOutcomeDetail").html(OutcomeDetail);
+        $("#closeButton").html(buttonText)
+        $('#exampleModalCenter').modal();
+        
     },
 
     /** Turns out drag/drop from JQueryUI doesn't have 
@@ -160,7 +180,7 @@ var objGame = {
      * This resets position based
      *      on original position settings stored at initialize.
     * */
-    newOpp: function (){
+    newOpp: function () {
         jqSelOpp = "#" + this.strSelOpp
         $(jqSelOpp).css({
             'left': $(jqSelOpp).data('originalLeft'),
@@ -169,16 +189,10 @@ var objGame = {
     },
     objOppPower: {},
     arrCharsPower: [{ power: 100, life: 200, },
-        { power: 150, life: 150 },
-        { power: 200, life: 200 },
-        { power: 250, life: 250 }
+    { power: 150, life: 150 },
+    { power: 200, life: 200 },
+    { power: 250, life: 250 }
     ],
-    // selectImgChar: function () {
-    //     this.selectOpponentChar();
-    // },
-    // selectOpponentChar: function () {
-    //     this.setupCharDrag(this.eleImgChars, this.eleOppChar);
-    // },  
 }
 
 objGame.setupPlayChars();
